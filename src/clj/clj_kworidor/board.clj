bc50ddb32a834ed9bc16d40003e253d6b9ae3302
Gary Fredericks <fredericksgary@gmail.com>
2013-10-23 10:36:09 -0500

    Chamges we made this morning as a group

--- src/clj/clj_kworidor/board.clj
+++ src/clj/clj_kworidor/board.clj
@@ -1,34 +1,30 @@
-(ns clj-kworidor.board)
-
-
-"
-Notation:
-                                                +-+
-                                                | |
-                                                |C|
-                                          C6    |6|    D6
-                                                |V|
- +-+-+-+-+-+-+-+-+-+                            | |
-9| | | | | | | | | |                 +----------+-+----------+
- +-+-+-+-+-+-+-+-+-+                 |         C6H           |
-8| | | | | | | | | |                 +----------+-+----------+
- +-+-+-+-+-+-+-+-+-+                            | |
-7| | | | | | | | | |                            | |
- +-+-+-+-+-+-+-+-+-+                      C5    | |    D5
-6| | | | | | | | | |                            | |
- +-+-+-+-+-+-+-+-+-+                            | |
-5| | | | | | | | | |                            +-+
- +-+-+-+-+-+-+-+-+-+
-4| | | | | | | | | |
- +-+-+-+-+-+-+-+-+-+
-3| | | | | | | | | |
- +-+-+-+-+-+-+-+-+-+
-2| | | | | | | | | |
- +-+-+-+-+-+-+-+-+-+
-1| | | | | | | | | |
- +-+-+-+-+-+-+-+-+-+
-  A B C D E F G H I
-"
+(ns clj-kworidor.board
+  "Notation:
+                                                  +-+
+                                                  | |
+                                                  |C|
+                                            C6    |6|    D6
+                                                  |V|
+   +-+-+-+-+-+-+-+-+-+                            | |
+  9| | | | | | | | | |                 +----------+-+----------+
+   +-+-+-+-+-+-+-+-+-+                 |         C6H           |
+  8| | | | | | | | | |                 +----------+-+----------+
+   +-+-+-+-+-+-+-+-+-+                            | |
+  7| | | | | | | | | |                            | |
+   +-+-+-+-+-+-+-+-+-+                      C5    | |    D5
+  6| | | | | | | | | |                            | |
+   +-+-+-+-+-+-+-+-+-+                            | |
+  5| | | | | | | | | |                            +-+
+   +-+-+-+-+-+-+-+-+-+
+  4| | | | | | | | | |
+   +-+-+-+-+-+-+-+-+-+
+  3| | | | | | | | | |
+   +-+-+-+-+-+-+-+-+-+
+  2| | | | | | | | | |
+   +-+-+-+-+-+-+-+-+-+
+  1| | | | | | | | | |
+   +-+-+-+-+-+-+-+-+-+
+    A B C D E F G H I")
 
 
 (defn column-label [index] (char (+ 97 index)))
@@ -42,17 +38,25 @@ Notation:
   [player-index]
   (< player-index 2))
 
+(defn span-and-distance
+  "Returns [span distance]."
+  [player-index width height]
+  (if (vertical-oriented-player? player-index)
+    [width height]
+    [height width]))
+
 (defn get-start-pos
   [player-index width height]
-  (let [span     (if (vertical-oriented-player? player-index) width height )
-        distance (if (vertical-oriented-player? player-index) height width )]
-  ((if (vertical-oriented-player? player-index) identity reverse ) [(int (dec (/ span 2))) (if (= (mod player-index 2) 0) 0 (dec distance))])))
+  (let [[span distance] (span-and-distance player-index width height)]
+    (cond-> [(int (dec (/ span 2))) (if (= (mod player-index 2) 0) 0 (dec distance))]
+
+            (vertical-oriented-player? player-index) (reverse))))
 
 (defn get-goal-pos
   [player-index width height]
-  (let [span     (if (vertical-oriented-player? player-index) width height )
-        distance (if (vertical-oriented-player? player-index) height width )]
-  (map (fn [x] ((if (vertical-oriented-player? player-index) identity reverse ) [ x (if (= (mod player-index 2) 0) (dec distance) 0) ] ) )(range span))))
+  (let [[span distance] (span-and-distance player-index width height)]
+    (map (fn [x] ((if (vertical-oriented-player? player-index) identity reverse ) [ x (if (= (mod player-index 2) 0) (dec distance) 0)]))
+         (range span))))
 
 
 (defn default-board
@@ -60,35 +64,38 @@ Notation:
   (let [width (or width 9)
         height (or height 9)
         num-players (or num-players 2)]
-    { :width width
-      :height height
-      :tiles-available 10
-      :player-moves (repeat num-players [])
-      :player-start-pos (map #( get-start-pos % width height ) (range num-players))
-      :player-goal-pos  (map #( get-goal-pos  % width height ) (range num-players))
-      }))
+    {:width width
+     :height height
+     :tiles-available 10
+     :player-moves (repeat num-players [])
+     :player-start-pos (map #(get-start-pos % width height) (range num-players))
+     :player-goal-pos  (map #(get-goal-pos  % width height) (range num-players))}))
 
 (def standard-two-player-board
   (default-board 7 11))
 
 (defn board-possible-movement-moves [board]
-  (for [x (range (:width board)) y (range (:height board))]
+  (for [x (range (:width board))
+        y (range (:height board))]
     [x y]))
 
 (defn board-possible-tile-moves [board]
   (concat
-    (for [x (range (dec (:width board))) y (range (dec (:height board)))]
+    (for [x (range (dec (:width board)))
+          y (range (dec (:height board)))]
       [x (inc y) :h])
-    (for [x (range (dec (:width board))) y (range (dec (:height board)))]
+    (for []
       [x (inc y) :v])))
 
 (defn current-player-pos
-  [board]
-  (let [[s1 s2] (:player-start-pos board) [m1 m2] (:player-moves board)]
-        [
-         (last (filter #(= (count %) 2) (cons s1 m1)))
-         (last (filter #(= (count %) 2) (cons s2 m2)))
-        ]))
+  "Takes a board and gives you back I suck because I'm a docstring."
+  [{:keys [player-start-pos player-moves]}]
+  (map (fn [start-square moves]
+         (->> (cons start-square moves)
+              (filter #(= 2 (count %)))
+              (last)))
+       player-start-pos
+       player-moves))
 
 standard-two-player-board
 
